291666779bb91c35c09ab32336c09645ea2f73f8
asder707 <abn.sebick@gmail.com>
2018-01-10 15:10:17 -0300

    se inicia proyecto junto a modelos, servicios y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {routing, appRoutingProviders} from './app.routing';
+
+import {AlbumsListComponent} from './components/albums-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumsListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing //es un módulo del framework
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
